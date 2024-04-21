0357e23a8c3583871667add60ef28792a86af57c
Dylan Carver <dylc2770@gmail.com>
2024-04-21 19:52:31 +0200

    feat: use gruvbox colors

--- dmenu/config.def.h
+++ dmenu/config.def.h
@@ -9,12 +9,12 @@ static const char *fonts[] = {
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
+	[SchemeNorm] = { "#bbbbbb", "#111111" },
+	[SchemeSel] = { "#000000", "#689d6a" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
-static unsigned int lines      = 0;
+static unsigned int lines      = 10;
 
 /*
  * Characters not considered part of a word while deleting words
--- dmenu/config.h
+++ dmenu/config.h
@@ -10,7 +10,7 @@ static const char *prompt      = NULL;      /* -p  option; prompt to the left of
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { "#bbbbbb", "#111111" },
-	[SchemeSel] = { "#000000", "#7AA2F7" },
+	[SchemeSel] = { "#000000", "#689d6a" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
